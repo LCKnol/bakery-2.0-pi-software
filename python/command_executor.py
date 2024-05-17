f20e2d79ae150a77cb904f488dcee3b55eaedd0b
Eduardo-ict <141565377+Ewawo@users.noreply.github.com>
2024-05-17 21:46:57 +0200

    x

--- python/command_executor.py
+++ python/command_executor.py
@@ -1,5 +1,8 @@
 from subprocess import call 
+import threading
 
 def execute_command(command: str) -> None:
-
-    call(command, shell=True)
+    thread = threading.Thread(target=call(command, shell=True))
+    thread.start
+    return
+    
