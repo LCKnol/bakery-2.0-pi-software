e153879c969fa2a859aa66d5a430f489631d88e8
Luc Knol <LC.Knol@student.han.nl>
2024-05-21 17:03:33 +0200

    ✨: Use actual IP/MAC addresses instead of placeholders

--- python/ip_address.py
+++ python/ip_address.py
@@ -1,4 +1,16 @@
+from netifaces import ifaddresses
+from subprocess import check_output
+
+
 def get_ip_address():
 
-    # TODO zorg dat dit het daadwerkelijke ip_address ophaalt van de Pi
-    return "248.69.69.132"
+    ifaceName = check_output(
+        "ip route show 0.0.0.0/0 | head -n 1 | awk '{print $5}'", shell=True).decode().strip()
+    ipAddress = ifaddresses(ifaceName).get(2)[0]['addr']
+
+    print()
+    print("name of first default network interface: ", ifaceName)
+    print("IP address of this interface:            ", ipAddress)
+    print()
+
+    return ifaddresses('wlp3s0').get(2)[0]['addr']
--- python/mac_address.py
+++ python/mac_address.py
@@ -1,5 +1,16 @@
+from netifaces import ifaddresses
+from subprocess import check_output
+
+
 def get_mac_address():
 
-    # TODO zorg dat dit de daadwerkelijke mac_address ophaalt van de PI
-    return "test:mac:address:7"
+    ifaceName = check_output(
+        "ip route show 0.0.0.0/0 | head -n 1 | awk '{print $5}'", shell=True).decode().strip()
+    macAddress = ifaddresses(ifaceName).get(17)[0]['addr']
 
+    print()
+    print("name of first default network interface: ", ifaceName)
+    print("MAC address of this interface:           ", macAddress)
+    print()
+
+    return macAddress
