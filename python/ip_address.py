830c430b8bc7b1e508da8dadb99cbab0a920a779
Bastiaan Hopman <BJ.Hopman@student.han.nl>
2024-05-22 16:53:55 +0200

    🐛 Quickfix

--- python/ip_address.py
+++ python/ip_address.py
@@ -4,14 +4,14 @@ from subprocess import check_output
 
 def get_ip_address():
 
-    # iface_name = check_output(
-    #     "ip route show 0.0.0.0/0 | head -n 1 | awk '{print $5}'", shell=True).decode().strip()
-    # ip_address = ifaddresses(iface_name).get(2)[0]['addr']
-    #
-    # print()
-    # print("name of first default network interface: ", iface_name)
-    # print("IP address of this interface:            ", ip_address)
-    # print()
+    iface_name = check_output(
+        "ip route show 0.0.0.0/0 | head -n 1 | awk '{print $5}'", shell=True).decode().strip()
+    ip_address = ifaddresses(iface_name).get(2)[0]['addr']
+    
+    print()
+    print("name of first default network interface: ", iface_name)
+    print("IP address of this interface:            ", ip_address)
+    print()
 
-    # return ip_address
-    return "1.2.3.4"
+    return ip_address
+    
--- python/mac_address.py
+++ python/mac_address.py
@@ -4,14 +4,13 @@ from subprocess import check_output
 
 def get_mac_address():
 
-    # iface_name = check_output(
-    #     "ip route show 0.0.0.0/0 | head -n 1 | awk '{print $5}'", shell=True).decode().strip()
-    # mac_address = ifaddresses(iface_name).get(17)[0]['addr']
-    #
-    # print()
-    # print("name of first default network interface: ", iface_name)
-    # print("MAC address of this interface:           ", mac_address)
-    # print()
-    #
-    # return mac_address
-    return "mac_address"
+    iface_name = check_output(
+        "ip route show 0.0.0.0/0 | head -n 1 | awk '{print $5}'", shell=True).decode().strip()
+    mac_address = ifaddresses(iface_name).get(17)[0]['addr']
+    
+    print()
+    print("name of first default network interface: ", iface_name)
+    print("MAC address of this interface:           ", mac_address)
+    print()
+    
+    return mac_address
