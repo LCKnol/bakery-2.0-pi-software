b9d88cb6bd1acc12ec649eb39ded556df59ad0b3
Luc Knol <LC.Knol@student.han.nl>
2024-05-24 13:09:14 +0200

    🐛: Unset refresh rate variable when 0

--- python/refresh_rate.py
+++ python/refresh_rate.py
@@ -2,4 +2,7 @@ from command_executor import execute_command
 
 
 def set_refresh_rate(refresh_rate: int):
-    execute_command(f'export BROWSER_REFRESH_RATE={int(refresh_rate)}')
+    if(refresh_rate > 0):
+        execute_command(f'export BROWSER_REFRESH_RATE={int(refresh_rate)}')
+    else:
+        execute_command(f'unset BROWSER_REFRESH_RATE')
