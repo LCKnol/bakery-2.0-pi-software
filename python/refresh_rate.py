3a45964e20dea36eae3669357aca55580c3f1baf
Luc Knol <LC.Knol@student.han.nl>
2024-05-27 14:58:08 +0200

    ⚡️: Update refresh rate without waiting for next refresh (interrupt sleep)

--- python/refresh_rate.py
+++ python/refresh_rate.py
@@ -1,5 +1,9 @@
+from command_executor import execute_command
+
+
 def set_refresh_rate(refresh_rate: int):
     file_path = "/home/colossuspi/Documents/refresh_rate.txt"
 
     with open(file_path, "w") as file:
         file.write(str(refresh_rate))
+        execute_command("sudo systemctl restart refreshd.service")
